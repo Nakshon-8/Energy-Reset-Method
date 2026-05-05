fd37a62b13505b0fc3f7348c80368650d06d8757
Dragan Maličević <serpentas9@gmail.com>
2026-05-05 17:47:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,13 @@
-const CACHE = 'erm-v6';
-
-const FILES = [
-  'app.html',
-  'manifest.json',
-  'sw.js',
-  'icon-192.png',
-  'icon-512.png',
-  'badge-final.png',
-  'badge-light.png'
-];
+const CACHE = 'erm-v10';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
@@ -29,6 +16,5 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
-  
   );
 });
